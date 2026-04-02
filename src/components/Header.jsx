aa31a9ea233f657ec6000f0c2a39dade82901ccb
azure <azure4201k@gmail.com>
2026-04-02 11:23:29 +0900

    04 test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
       <div className="header_inner">
         <div className="logo">
           <a href="#">
-            EDIYA AA <em>coffee</em>
+            EDIYA <em>coffee</em>
           </a>
         </div>
         <div className="header_menu">
